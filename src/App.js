be8358af4edd2062a181a861b4f389f0d2862692
Maycon Benito <mayki17321@gmail.com>
2020-01-14 13:54:28 -0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          data-testid="headerImg"
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          data-testid="link"
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("expect image to hava src and alt tags", () => {
+  const { getByTestId } = render(<App />);
+  const img = getByTestId("headerImg");
+
+  expect(img).toHaveAttribute("src");
+  expect(img).toHaveAttribute("alt");
+});
+
+test("expect App to have a link to https://reactjs.org", () => {
+  const { getByTestId } = render(<App />);
+  const link = getByTestId("link");
+
+  expect(link).toHaveAttribute("href", "https://reactjs.org");
 });
